10b183b773e45f01846fc34a1879c5ade0b91372
junmin-Chang <chjm219@kookmin.ac.kr>
2025-01-16 16:45:56 +0900

    dlopen

--- make_lib/separate_module/shared_lib/dlopen/main.c
+++ make_lib/separate_module/shared_lib/dlopen/main.c
@@ -5,15 +5,15 @@ int sum(int p1, int p2);
 
 int main(void) {
     void *handle;
-    int (*sum)(int, int);
+    int (*sum_ptr)(int, int);
     handle = dlopen("./libsum.so", RTLD_LAZY);
     if (!handle) {
         fprintf(stderr, "%s\n", dlerror());
     }
 
-    sum = dlsym(handle, "sum");
+    sum_ptr = dlsym(handle, "sum");
 
-    printf("SUM: %d\n", (*sum)(3,4));
+    printf("SUM: %d\n", (*sum_ptr)(3,4));
 
     return 0;
 }
